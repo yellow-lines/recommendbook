a294735763f71b16b669af7d17d91bef10025867
AsianMiracle <filon_len.0912@mail.ru>
2021-10-23 23:18:58 +0300

    settings

--- recommender_book/settings.py
+++ recommender_book/settings.py
@@ -69,13 +69,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'recommender_users',
-<<<<<<< HEAD
-        'USER': '',
-        'PASSWORD': '',
-=======
         'USER': 'postgres',
         'PASSWORD': 'sleeperonelove',
->>>>>>> graph_output
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
